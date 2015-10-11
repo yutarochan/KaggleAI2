48fc926a87ed3c4b504d52c32f5a0cba59a4a936
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2015-10-10 22:01:32 -0400

    Append preliminary nltk analysis

--- prelimAnalysis.py
+++ prelimAnalysis.py
@@ -1,7 +1,22 @@
 import nltk
 import fileUtil
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
+# Just a quick test of some of NLTK's functionality.
 
 ques = fileUtil.parseTrainingSet('data/training_set.tsv')
 
-token = nltk.word_tokenize(ques[0].question)
-print token
+# Tokenize and Remove Punctuation (Using regex tokenizer)
+tokenizer = RegexpTokenizer(r'\w+')
+token = tokenizer.tokenize(ques[0].question)
+
+# Stop Word Removal
+word_list = token[:]
+stops = stopwords.words('english')
+filtered_word_list = word_list[:] #make a copy of the word_list
+for word in word_list: # iterate over word_list
+  if word in stops:
+    filtered_word_list.remove(word)
+
+print filtered_word_list
