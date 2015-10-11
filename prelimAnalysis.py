e2111302e7cd64fba97b28e847ebaa975013b0c7
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2015-10-11 01:22:46 -0400

    Update files

--- prelimAnalysis.py
+++ prelimAnalysis.py
@@ -5,28 +5,33 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import *
 from nltk.stem.snowball import SnowballStemmer
 
-# Just a quick test of some of NLTK's functionality.
+def preprocess(text)
+    # Tokenize and Remove Punctuation (Using regex tokenizer)
+    tokenizer = RegexpTokenizer(r'\w+')
+    token = tokenizer.tokenize(text)
 
-ques = fileUtil.parseTrainingSet('data/training_set.tsv')
+    # Stop Word Removal
+    word_list = token[:]
+    stops = stopwords.words('english')
+    filtered_word_list = word_list[:] #make a copy of the word_list
+    for word in word_list: # iterate over word_list
+      if word in stops:
+        filtered_word_list.remove(word)
 
-# Tokenize and Remove Punctuation (Using regex tokenizer)
-tokenizer = RegexpTokenizer(r'\w+')
-token = tokenizer.tokenize(ques[0].question)
+    # Stemming Variation 1: Porter Stemmer
+    # stemmer = PorterStemmer()
+    # stemmed = [stemmer.stem(plural) for plural in filtered_word_list]
+    # print stemmed
 
-# Stop Word Removal
-word_list = token[:]
-stops = stopwords.words('english')
-filtered_word_list = word_list[:] #make a copy of the word_list
-for word in word_list: # iterate over word_list
-  if word in stops:
-    filtered_word_list.remove(word)
+    # Stemming Variation 2: Snowball Stemmer
+    stemmer = SnowballStemmer("english")
+    stemmed = [stemmer.stem(plural) for plural in filtered_word_list]
 
-# Stemming Variation 1: Porter Stemmer
-# stemmer = PorterStemmer()
-# stemmed = [stemmer.stem(plural) for plural in filtered_word_list]
-# print stemmed
+    return stemmed
 
-# Stemming Variation 2: Snowball Stemmer
-stemmer = SnowballStemmer("english")
-stemmed = [stemmer.stem(plural) for plural in filtered_word_list]
-print stemmed
+# Unit Testing
+if __name__ == "__main__":
+    ques = fileUtil.parseTrainingSet('data/training_set.tsv')
+
+    # Testing with just sample data...
+    preprocess
